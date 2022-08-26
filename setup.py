711ceb8b64c7a32975b07a5962ae1d5a82c7e72a
Gautam Mittal <gautam@mittal.net>
2022-08-25 17:20:27 -0700

    rename package

--- setup.py
+++ setup.py
@@ -8,17 +8,18 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='lambda',
-    version='1.0.0',
+    name='lambda-cli',
+    version='0.0.1',
     description="Lambda Labs instance management",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/gmittal/lambda-cli',
     author='Gautam Mittal',
     keywords='development',
-    packages=["lambda"],
-    package_data={'lambda': [ 'catalog.csv' ]},
-    entry_points={'console_scripts': ['lambda=lambda:main']},
+    packages=["lambda_labs"],
+    requires_python='>=3.6',
+    package_data={'lambda_labs': [ 'catalog.csv' ]},
+    entry_points={'console_scripts': ['lambda=lambda_labs:main']},
     install_requires=[
         'colorama', 'fire', 'jinja2', 'pandas', 'pendulum', 'petname', 'prettytable',
         'pyppeteer'
